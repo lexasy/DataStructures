886ca097158e2bc990e54abb453a27d7c8c7d3e2
lexxasy <lexav2016@mail.ru>
2023-03-30 16:35:27 +0300

    added not done double linked list

---
+++ lists/double-linked-list.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <stdlib.h>
+
+typedef struct node node;
+struct node {
+    int value;
+    node *next;
+    node *prev;
+};
+
+typedef struct {
+    node *head;
+    node *last;
+    int size;
+} double_linked_list;
+
+double_linked_list create_list();
+bool is_empty(double_linked_list *l);
+int size_of_list(double_linked_list *l);
+int first_elem(double_linked_list *l);
+int last_elem(double_linked_list *l);
+int next_elem(double_linked_list *l);
+int prev_elem(double_linked_list *l);
+
+
+
